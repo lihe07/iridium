0404e20b9fb3a313e88a29e29d9564cfa894d4c3
lihe07 <li@imlihe.com>
2022-11-29 19:22:33 +0800

    Better Prerender

--- src/routes/news/[...id].jsx
+++ src/routes/news/[...id].jsx
@@ -1,22 +1,14 @@
-import { createEffect } from 'solid-js'
-import { redirect, useRouteData } from 'solid-start'
-import { createServerData$ } from 'solid-start/server'
+import { createEffect, createResource } from 'solid-js'
+import { useRouteData } from 'solid-start'
 
 const api = 'https://www.notkiller.moe/wp-json/wp/v2/posts/'
 
 export function routeData ({ params }) {
-  return createServerData$(
-    async ([id]) => {
-      const resp = await fetch(`${api}${id}`)
-      // If 404
-      if (resp.status === 404) {
-        return redirect('/404')
-      }
-      return await resp.json()
-    },
-
-    { key: () => [params.id] }
-  )
+  return createResource(async () => {
+    const resp = await fetch(`${api}${params.id}`)
+    console.log('Data fetched from:', `${api}${params.id}`)
+    return await resp.json()
+  })[0]
 }
 
 export default () => {
@@ -28,6 +20,7 @@ export default () => {
     <div class="mt-20">
       <p>Todo: Style</p>
       <h1>{data()?.title?.rendered}</h1>
+      {/* eslint-disable-next-line solid/no-innerhtml */}
       <div innerHTML={data()?.content?.rendered} />
     </div>
   )
